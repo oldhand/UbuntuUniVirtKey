36f5a2b44ed72a3d7382004bc9dc5175a69a2259
oldhand <68594864@qq.com>
2025-09-21 15:40:18 +0800

    no message

--- install.sh
+++ install.sh
@@ -19,5 +19,16 @@ if [ ! -f "images/prometheus-v2.45.0.tar" ]; then
 fi
 
 
+
+if [ ! -f "ceph/quay.io-rook-ceph-v1.18.2.tar" ]; then
+  cat $PWD/ceph/ceph.zip.001 $PWD/ceph/ceph.zip.002 $PWD/ceph/ceph.zip.003 $PWD/ceph/ceph.zip.004 $PWD/ceph/ceph.zip.005 \
+      $PWD/ceph/ceph.zip.006 $PWD/ceph/ceph.zip.007 $PWD/ceph/ceph.zip.008 $PWD/ceph/ceph.zip.009 $PWD/ceph/ceph.zip.010 \
+      $PWD/ceph/ceph.zip.011 $PWD/ceph/ceph.zip.012 $PWD/ceph/ceph.zip.013 $PWD/ceph/ceph.zip.014 $PWD/ceph/ceph.zip.015 \
+      $PWD/ceph/ceph.zip.016 $PWD/ceph/ceph.zip.017 $PWD/ceph/ceph.zip.018 $PWD/ceph/ceph.zip.019 $PWD/ceph/ceph.zip.020 \
+      $PWD/ceph/ceph.zip.021 $PWD/ceph/ceph.zip.022 $PWD/ceph/ceph.zip.023 > $PWD/ceph/ceph.zip
+  unzip $PWD/ceph/ceph.zip -d $PWD/ceph/
+  rm -fr $PWD/ceph/ceph.zip
+fi
+
 #ansible-playbook -i hosts.ini install.yml -k
 ansible-playbook -i hosts.ini install.yml
